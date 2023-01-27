28f613d375d74f72f565993e6a00a5b5c54db5c9
Crowdbotics <engineering@crowdbotics.com>
2023-01-27 00:06:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-flower-dev-59466",
+  url: "https://divine-flower-dev-59466.botics.co",
+  api: "https://divine-flower-dev-59466.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
